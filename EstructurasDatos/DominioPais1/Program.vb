6b61f7ab0106a3ce3dcbae1d77f5564b393f9576
Usuario <Usuario@NENONCHI>
2019-08-03 21:02:55 -0300

    4.4

---
+++ EstructurasDatos/DominioPais1/Program.vb
@@ -0,0 +1,21 @@
+Imports System
+
+Module DominioPais
+    Dim Coleccion As New Collection
+
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+    End Sub
+    Sub Dominio()
+
+
+
+
+    End Sub
+
+End Module
+
+'4.4. Crear un proyecto llamado “DominioPais” y un módulo que cargue programáticamente
+'nombre de países y su dominio de país en una Collection. A continuación, debe permitir
+'realizar consultas: se introduce el nombre del dominio y se deberá mostrar el nombre del
+'país.Salir cuando el valor ingresado es vacío.
---
+++ EstructurasDatos/DominioPais2/Module1.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+    Private Coleccion As New Collection
+
+    Sub Main()
+        Paises()
+        Dim Dom As String
+        Do
+            Console.Write("Ingrese el dominio('ar' para Argentina: ")
+            Dom = Console.ReadLine()
+            If Coleccion.Contains(Dom) Then
+                Console.WriteLine("Pais: " & Coleccion(Dom))
+            End If
+
+        Loop While Dom <> ""
+    End Sub
+    Sub Paises()
+        Coleccion.Add("Argentina", "ar")
+        Coleccion.Add("Brasil", "br")
+        Coleccion.Add("Uruguay", "uy")
+        Coleccion.Add("Australia", "au")
+        Coleccion.Add("Bolivia", "bo")
+        Coleccion.Add("Francia", "fr")
+        Coleccion.Add("España", "es")
+        Coleccion.Add("Paraguay", "py")
+
+    End Sub
+
+
+
+End Module
+
+'4.4. Crear un proyecto llamado “DominioPais” y un módulo que cargue programáticamente
+'nombre de países y su dominio de país en una Collection. A continuación, debe permitir
+'realizar consultas: se introduce el nombre del dominio y se deberá mostrar el nombre del
+'país.Salir cuando el valor ingresado es vacío.
